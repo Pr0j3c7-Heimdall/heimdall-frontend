028e2216ba99585e5066d5851663a82ea122d990
yeeun0702 <kimyeeun0702@naver.com>
2026-02-07 16:50:01 +0900

    [Feat](auth): GoogleOAuthProvider 컴포넌트
    - clientId 미설정 시 children만 반환

---
+++ src/app/GoogleOAuthProvider.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+
+export default function AppGoogleOAuthProvider({ children }) {
+  if (!clientId) {
+    return children;
+  }
+
+  return <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>;
+}
