c00ba9fc64287bf7d1a56969ff3af5997c294109
yeeun0702 <kimyeeun0702@naver.com>
2026-02-07 16:50:02 +0900

    [Feat](auth): layout에 AuthModalProvider 래핑

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,15 @@
 import './globals.css';
+import AppGoogleOAuthProvider from './GoogleOAuthProvider';
+import AuthModalProvider from '@/components/AuthModal/AuthModalProvider';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <AppGoogleOAuthProvider>
+          <AuthModalProvider>{children}</AuthModalProvider>
+        </AppGoogleOAuthProvider>
+      </body>
     </html>
   );
 }
