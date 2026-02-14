5fdfa41a9eb5602792c4a7082d4c35593a5ff14a
yeeun0702 <kimyeeun0702@naver.com>
2026-02-14 15:47:33 +0900

    [Feat](auth): AuthProvider 루트 레이아웃 적용

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,16 @@
 import './globals.css';
 import AppGoogleOAuthProvider from './GoogleOAuthProvider';
 import AuthModalProvider from '@/components/AuthModal/AuthModalProvider';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
         <AppGoogleOAuthProvider>
-          <AuthModalProvider>{children}</AuthModalProvider>
+          <AuthProvider>
+            <AuthModalProvider>{children}</AuthModalProvider>
+          </AuthProvider>
         </AppGoogleOAuthProvider>
       </body>
     </html>
