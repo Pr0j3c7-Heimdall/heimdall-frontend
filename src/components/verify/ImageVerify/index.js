a7deb0a050ad14784370fc1527e2760e63e92bb0
yeeun0702 <kimyeeun0702@naver.com>
2026-02-21 11:11:58 +0900

    [Chore](index.js): ImageVerifyResult export 추가
    - ImageVerifyResult 컴포넌트 export

--- src/components/verify/ImageVerify/index.js
+++ src/components/verify/ImageVerify/index.js
@@ -1,2 +1,3 @@
 export { default as ImageDropzone } from './ImageDropzone';
 export { default as ImageVerifyContent } from './ImageVerifyContent';
+export { default as ImageVerifyResult } from './ImageVerifyResult';
