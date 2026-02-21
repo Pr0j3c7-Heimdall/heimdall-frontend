8d620730c90795dec948d49eac347073d6c66382
yeeun0702 <kimyeeun0702@naver.com>
2026-02-21 11:09:27 +0900

    [Chore](layout.jsx): metadataConfig 적용
    - layout.jsx에서 metadataConfig import 및 metadata export

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,9 @@ import './globals.css';
 import AppGoogleOAuthProvider from './GoogleOAuthProvider';
 import AuthModalProvider from '@/components/AuthModal/AuthModalProvider';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { metadataConfig } from '@/config/metadata';
+
+export const metadata = metadataConfig;
 
 export default function RootLayout({ children }) {
   return (
