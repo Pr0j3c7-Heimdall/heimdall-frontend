da037fafbd291bbfa0dc218d8bf5f6283a6047ef
yeeun0702 <kimyeeun0702@naver.com>
2026-02-24 03:25:14 +0900

    [Chore](AudioVerify/index.js): AudioVerifyGuide export 추가
    - export default AudioVerifyGuide

--- src/components/verify/AudioVerify/index.js
+++ src/components/verify/AudioVerify/index.js
@@ -1,2 +1,3 @@
 export { default as AudioDropzone } from './AudioDropzone';
 export { default as AudioVerifyContent } from './AudioVerifyContent';
+export { default as AudioVerifyGuide } from './AudioVerifyGuide';
